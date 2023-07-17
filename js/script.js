2e2e18e45fc41ba99f222faadedb46fdfd6b12e0
Peter Chapman <pchapman@ptc.com>
2023-07-17 10:42:49 -0700

    Movie details page build.

--- js/script.js
+++ js/script.js
@@ -34,6 +34,64 @@ async function displayPopularMovies() {
   });
 }
 
+async function displayMovieDetails() {
+  const movieId = window.location.search.split("=")[1];
+  const movie = await fetchAPIData(`movie/${movieId}`);
+
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <div class="details-top">
+    <div>
+    ${movie.poster_path
+      ? `<img
+        src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+        class="card-img-top"
+        alt="${movie.title}"
+      />`
+      : `<img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt="${movie.title}"
+        />`
+    }
+    </div>
+    <div>
+      <h2>${movie.title}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${movie.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Release Date: ${movie.release_date}</p>
+      <p>${movie.overview}</p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${movie.genres.map((genre) => {
+          return `
+          <li>${genre.name}</li>
+          `}).join('')}
+      </ul>
+      <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Movie Info</h2>
+    <ul>
+      <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+      <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+      <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+      <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+      ${movie.production_companies.map((company) => {
+        return `<span>${company.name}</span>`
+      }).join(', ')}
+    </div>
+  </div>
+  `;
+  document.querySelector("#movie-details").appendChild(div);
+}
+
 // Display 20 ost popular tv shows
 async function displayPopularShows() {
   const { results } = await fetchAPIData("tv/popular");
@@ -100,6 +158,10 @@ function highlightActiveLink() {
   });
 }
 
+function addCommasToNumber(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 // init app
 function init() {
   switch (global.currentPage) {
@@ -111,7 +173,7 @@ function init() {
       displayPopularShows();
       break;
     case "/movie-details.html":
-      console.log("Movie details");
+      displayMovieDetails();
       break;
     case "/tv-details.html":
       console.log("TV details");
