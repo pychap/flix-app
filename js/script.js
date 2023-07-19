f5d2d7bb91d82e7439524684f2106e150d2555c7
Peter Chapman <pchapman@ptc.com>
2023-07-19 11:39:58 -0700

    Display TV Show details page

--- js/script.js
+++ js/script.js
@@ -109,7 +109,7 @@ async function displayMovieDetails() {
   document.querySelector("#movie-details").appendChild(div);
 }
 
-// Display 20 ost popular tv shows
+// Display 20 most popular tv shows
 async function displayPopularShows() {
   const { results } = await fetchAPIData("tv/popular");
   results.forEach((show) => {
@@ -140,6 +140,75 @@ async function displayPopularShows() {
     `;
     document.querySelector("#popular-shows").appendChild(div);
   });
+}
+
+// TV Show details
+async function displayShowDetails() {
+  const showId = window.location.search.split("=")[1];
+  const show = await fetchAPIData(`tv/${showId}`);
+
+  // Overlay for bkg image
+  displayBackgroundImage("tv", show.backdrop_path);
+
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <div class="details-top">
+    <div>
+    ${
+      show.poster_path
+        ? `<img
+        src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+        class="card-img-top"
+        alt="${show.name}"
+      />`
+        : `<img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt="${show.name}"
+        />`
+    }
+    </div>
+    <div>
+      <h2>${show.name}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${show.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+      <p>${show.overview}</p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${show.genres
+          .map((genre) => {
+            return `
+          <li>${genre.name}</li>
+          `;
+          })
+          .join("")}
+      </ul>
+      <a href="${
+        show.homepage
+      }" target="_blank" class="btn">Visit TV Show Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Show Info</h2>
+    <ul>
+      <li><span class="text-secondary">Number of episodes:</span> ${show.number_of_episodes}</li>
+      <li><span class="text-secondary">Last episode to air:</span> ${show.last_episode_to_air.name}</li>
+      <li><span class="text-secondary">Status:</span> ${show.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+      ${show.production_companies
+        .map((company) => {
+          return `<span style="display: block;">${company.name}</span>`;
+        })
+        .join("")}
+    </div>
+  </div>
+  `;
+  document.querySelector("#show-details").appendChild(div);
 }
 
 // Fetch data from TMDB API
@@ -215,7 +284,7 @@ function init() {
       displayMovieDetails();
       break;
     case "/tv-details.html":
-      console.log("TV details");
+      displayShowDetails();
       break;
     case "/search.html":
       console.log("Search");
